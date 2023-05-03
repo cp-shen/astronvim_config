686027f3a1fa1e60c28331f92677af8cc33025b4
scp <scp@archlinuxscp.xyz>
2023-05-04 02:48:54 +0800

    add lua_ls

--- init.lua
+++ init.lua
@@ -50,8 +50,33 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      "lua_ls"
     },
+
+    config = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'},
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }
+    }
   },
 
   -- Configure require("lazy").setup() options
